b3d8b3cc2ab82b3c51992a1e05f282d647083279
ganesh7890 <gsakle@bestpeers.com>
2022-06-13 19:03:00 +0530

    Login form added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   root 'main#index'
   #root 'abouts#index'
-  get "/about", to: "about#index"
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
