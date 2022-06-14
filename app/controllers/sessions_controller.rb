10213c367d9aa6c05044382745f3b667a17c3e2e
ganesh7890 <gsakle@bestpeers.com>
2022-06-14 16:41:50 +0530

    Login authentication added now signuped user can logged-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to user
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  root 'main#index'
+  root 'users#new'
   #root 'abouts#index'
-  get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
+  get  '/signup', to: 'users#new'
+  get  '/login',  to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
