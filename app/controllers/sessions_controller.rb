3fb7e4516a7e213699b1011b258cb36620a4334f
ganesh7890 <gsakle@bestpeers.com>
2022-06-15 13:04:10 +0530

    new navbar added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       redirect_to user
     else
+      flash.now[:danger]="Invalid email/password combination"
       render "new"
     end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+
+    # log in the given user
+    def log_in(user)
+        session[:user_id]=user.id
+    end
+
+    # Returns the current Logged-in user (if any?) 
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end 
+    end
+
+    # Returns true if the user is logged in, false otherwise.
+    def logged_in?
+        !current_user.nil?  #checking-> current_user is not nil
+    end
+
 end
