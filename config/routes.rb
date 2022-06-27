c7f34af1639d9b80e1efb4acf268f47e01dd3687
ganeshsakle <ganeshsakle7@gmail.com>
2022-06-27 15:25:29 +0530

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  root 'users#index'
+  root to: 'users#index'
   get  '/signup', to: 'users#new'
   get  '/login',  to: 'sessions#new'
   post '/login',  to: 'sessions#create'
